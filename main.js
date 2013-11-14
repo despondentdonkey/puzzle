a29752bc66219c362b87955e2c0dcc5a75f00431
Parker Miller <despondentdonkey@gmail.com>
2013-11-14 15:15:15 -0700

    Initial

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    var img = $("#img");
+    img.style.visiblility = "visible";
+    var div = $("#puzzle");
+
+    $(div).html(img);
+    console.log("hello world!");
+});
