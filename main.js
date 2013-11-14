f9375a818276a914875da7fb7155b52ed9229e11
dogpantz <dogpantz@gmail.com>
2013-11-14 16:48:44 -0700

    Added board generation

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
     var img = $("#img");
-    img.style.visiblility = "visible";
+    //img.style.visiblility = "visible";
     var div = $("#puzzle");
 
     $(div).html(img);
     console.log("hello world!");
+
+    var pieces = ['<table>'];
+    var w = 400;
+    var h = 300;
+    for (var i = 0; i < w; i+=100) {
+        pieces.push('<tr>');
+        for (var j = 0; j < h; j+=100) {
+            pieces.push('<td class="puzzle" style="background-position: '+-i+'px '+-j+'px"></div>');
+        }
+    }
+    pieces.push('</table');
+
+    console.log(pieces);
+    $(div).append(pieces.join(''));
+
 });
