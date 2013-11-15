4b6d13903a16f2d0d010d6e8d59a2766b296a6c7
Parker Miller <despondentdonkey@gmail.com>
2013-11-14 19:03:33 -0700

    Converted to canvas to handle slices.

--- main.js
+++ main.js
@@ -1,27 +1,52 @@
 $(document).ready(function() {
-    var img = $("#img");
+    $("#img").load(function() {
+        start();
+    });
+});
+
+function start() {
     var div = $("#puzzle");
 
-    $(div).html(img);
-
-    // Tile Size
-    var w = 4;
-    var h = 3;
-    // Image Size
-    var iw = 400;
-    var ih = 300;
-    // Piece Size
-    var pw = 100;
-    var ph = 100;
     var pieces = ['<table>'];
-    for (var i = 0; i < h; i++) {
+
+    //Slice count
+    var hslices = 4;
+    var vslices = 3;
+
+    //Base image and the urls of the sliced images.
+    var img = $("#img")[0];
+    var imgURLS = [[]];
+
+    //The width/height of each slice.
+    var sliceWidth = img.width / hslices;
+    var sliceHeight = img.height / vslices;
+
+    for (var j=0; j<vslices; ++j) {
+        imgURLS[j] = [];
+
+        for (var i=0; i<hslices; ++i) {
+            var canvas = document.createElement('canvas');
+            canvas.width = sliceWidth;
+            canvas.height = sliceHeight;
+
+            var gc = canvas.getContext('2d');
+
+            gc.drawImage(img, i * sliceWidth, j * sliceHeight, sliceWidth, sliceHeight, 0, 0, canvas.width, canvas.height);
+
+            var imgURL = canvas.toDataURL();
+            imgURLS[j][i] = imgURL;
+        }
+    }
+
+    for (var i = 0; i < vslices; i++) {
         pieces.push('<tr>');
-        for (var j = 0; j < w; j++) {
-            pieces.push('<td id="'+i+j+'" class="puzzle" style="background-position: '
-                +-(iw/pw * j)+'px '+-(ih/ph * i)+'px"></div>');
+        for (var j = 0; j < hslices; j++) {
+            pieces.push('<td>');
+                pieces.push("<img src='" + imgURLS[i][j] + "'></img>");
+            pieces.push('</td>');
         }
     }
     pieces.push('</table>');
 
     $(div).append(pieces.join(''));
-});
+}
