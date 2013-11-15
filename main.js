e1a159d81bf2d3af68836e4fd274a6a34989a806
dogpantz <dogpantz@gmail.com>
2013-11-14 19:56:38 -0700

    Cleaned up code, abstracted image/board creation
    Reduced number of canvas creations to one.
    Added game to namespace
    Added ids to tds and imgs

--- main.js
+++ main.js
@@ -1,52 +1,63 @@
 $(document).ready(function() {
-    $("#img").load(function() {
-        start();
+    $("#puzzle_img").load(function() {
+        PuzzleGame.start();
     });
 });
+var PuzzleGame = {
+    // Tile Width/Height
+    width: 4,
+    height: 3,
+    start: function() {
+        var div = $("#puzzle");
 
-function start() {
-    var div = $("#puzzle");
+        //Base image and the urls of the sliced images.
+        var img = $("#puzzle_img")[0];
 
-    var pieces = ['<table>'];
 
-    //Slice count
-    var hslices = 4;
-    var vslices = 3;
+        $(div).append(this.createBoard(this.createImages(img)));
+    },
 
-    //Base image and the urls of the sliced images.
-    var img = $("#img")[0];
-    var imgURLS = [[]];
-
-    //The width/height of each slice.
-    var sliceWidth = img.width / hslices;
-    var sliceHeight = img.height / vslices;
-
-    for (var j=0; j<vslices; ++j) {
-        imgURLS[j] = [];
-
-        for (var i=0; i<hslices; ++i) {
-            var canvas = document.createElement('canvas');
-            canvas.width = sliceWidth;
-            canvas.height = sliceHeight;
-
-            var gc = canvas.getContext('2d');
-
-            gc.drawImage(img, i * sliceWidth, j * sliceHeight, sliceWidth, sliceHeight, 0, 0, canvas.width, canvas.height);
-
-            var imgURL = canvas.toDataURL();
-            imgURLS[j][i] = imgURL;
+    createBoard: function(images) {
+        var w = this.width, h = this.height;
+        var pieces = ['<table id="puzzle_table">'];
+        for (var i = 0; i < h; i++) {
+            pieces.push('<tr>');
+            for (var j = 0; j < w; j++) {
+                pieces.push('<td id="puzzle_td_'+i+j+'">'+
+                    images[i][j]+'</td>');
+            }
         }
+        pieces.push('</table>');
+
+        return pieces.join('');
+    },
+
+    createImages: function(img) {
+        var w = this.width, h = this.height;
+
+        var images = [];
+
+        //The width/height of each slice.
+        var sliceWidth = img.width / w;
+        var sliceHeight = img.height / h;
+
+        var canvas = document.createElement('canvas');
+        var gc = canvas.getContext('2d');
+        canvas.width = sliceWidth;
+        canvas.height = sliceHeight;
+
+        for (var i=0; i<h; i++) {
+            images[i] = [];
+            for (var j=0; j<w; j++) {
+                gc.drawImage(img, j * sliceWidth, i * sliceHeight, sliceWidth, sliceHeight, 0, 0, canvas.width, canvas.height);
+
+                var imgURL = canvas.toDataURL();
+                images[i][j] = '<img src="'+imgURL+'" id="puzzle_img_'+i+j+'" />';
+                //gc.clearRect(0, 0, canvas.width, canvas.height); // necessary?
+            }
+        }
+
+        return images;
     }
 
-    for (var i = 0; i < vslices; i++) {
-        pieces.push('<tr>');
-        for (var j = 0; j < hslices; j++) {
-            pieces.push('<td>');
-                pieces.push("<img src='" + imgURLS[i][j] + "'></img>");
-            pieces.push('</td>');
-        }
-    }
-    pieces.push('</table>');
-
-    $(div).append(pieces.join(''));
-}
+};
